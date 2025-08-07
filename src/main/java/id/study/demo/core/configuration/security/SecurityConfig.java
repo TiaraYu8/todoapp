85f860a29ef496d0bcf7bcccc346532f9269be4c
TiaraYu8 <yuliandari.tiara@gmail.com>
2025-08-07 16:58:28 +0700

    adding / on securityConfig

--- src/main/java/id/study/demo/core/configuration/security/SecurityConfig.java
+++ src/main/java/id/study/demo/core/configuration/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     private static final String[] WHITELIST_URL = {
             "/session/login",
             "/session/check",
-            "users/register",
+            "/users/register",
            };
 
     @Bean
